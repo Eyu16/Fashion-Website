6d1a26388e31d0d7175a6c5c56363d6442bb738d
Eyu16 <eyobyihalem@gmail.com>
2024-10-24 11:56:29 -0700

    cart icon

--- src/components/ShopItems.jsx
+++ src/components/ShopItems.jsx
@@ -43,8 +43,30 @@ function ShopItems({ type, images }) {
               <img src={`/img/${img}`} alt="shop" />
             </figure>
           </Link>
-          <h4>Netela Dress</h4>
-          <span>250$</span>
+          <div className={Styles.description_cotainer}>
+            <div>
+              <h4 className={Styles.product_name}>Netela Dress</h4>
+              <span className={Styles.product_price}>250$</span>
+            </div>
+            <div className={Styles.cart_container}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="38"
+                height="38"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className={Styles.cart}
+              >
+                <circle cx="9" cy="21" r="1"></circle>
+                <circle cx="20" cy="21" r="1"></circle>
+                <path d="M1 1h4l2 13h13l2-9H6"></path>
+              </svg>
+            </div>
+          </div>
         </div>
       ))}
       {/*  */}
